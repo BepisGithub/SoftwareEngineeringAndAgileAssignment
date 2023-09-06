69892df2bdc3bfdef57d90f6a152dbef8666e5bd
Luqman Dekla <deklluqm@amazon.com>
2023-09-06 15:29:38 +0100

    Create user model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+# The abstract user class provides most of the base functionality needed for a user class, e.g. username, email, etc.
+# See here: https://docs.djangoproject.com/en/4.2/topics/auth/customizing/#django.contrib.auth.models.AbstractBaseUser
+
+
+class User(AbstractUser):
+
+    # The user should not be an admin by default
+    is_admin = models.BooleanField(default=False)
+
+
+
