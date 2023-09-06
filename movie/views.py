78f20018eebefb9630151b9e4173cb43690e92bf
Luqman Dekla <deklluqm@amazon.com>
2023-09-06 23:34:44 +0100

    Add basic display for individual movie

--- movie/views.py
+++ movie/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from .models import Movie
@@ -15,5 +15,12 @@ def index(request):
 
     return render(request, template_path, context)
 
+
 def display(request, id):
-    return HttpResponse("hello!")
+    movie = get_object_or_404(Movie, id=id)
+    template_path = 'movie/display.html'
+    context = {
+        'movie': movie
+    }
+
+    return render(request, template_path, context)
