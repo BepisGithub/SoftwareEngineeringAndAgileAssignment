24495b148e2ffcd8e75d6c0af5257f832b2b61f8
Luqman Dekla <deklluqm@amazon.com>
2023-09-06 21:27:35 +0100

    Add page to display all movies

--- movie/views.py
+++ movie/views.py
@@ -2,6 +2,15 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Movie
+
 
 def index(request):
-    return HttpResponse("<h1> Movies </h1>")
+    # TODO: add pagination
+    movies = Movie.objects.all()
+    template_path = 'movie/index.html'
+    context = {
+        'movies': movies
+    }
+
+    return render(request, template_path, context)
