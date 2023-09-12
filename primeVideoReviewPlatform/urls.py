3e84416235e11bb9f28f1642f70fb07b6532046f
Luqman Dekla <deklluqm@amazon.com>
2023-09-12 16:29:02 +0100

    Allow users to change their own passwords

--- primeVideoReviewPlatform/urls.py
+++ primeVideoReviewPlatform/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import PasswordChangeView
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
@@ -26,6 +27,7 @@ urlpatterns = [
     path('', include('movie.urls')),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('change-password/', PasswordChangeView.as_view(), name='change_password'),
     path('register/', register, name='register'),
     path('users/', include('user.urls'))
 
--- user/urls.py
+++ user/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
+from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -8,7 +9,6 @@ app_name = 'user'
 urlpatterns = [
     path('', views.UserListView.as_view(), name='users'),
     path('<int:pk>/', views.UserDetailView.as_view(), name='user'),
-    path('<int:pk>/update', views.UserUpdateView.as_view(), name='update_user'),
-    path('<int:pk>/delete', views.UserDeleteView.as_view(), name='delete_user')
-
+    path('<int:pk>/update/', views.UserUpdateView.as_view(), name='update_user'),
+    path('<int:pk>/delete/', views.UserDeleteView.as_view(), name='delete_user'),
 ]
