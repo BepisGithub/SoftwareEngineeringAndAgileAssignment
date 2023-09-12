fe894bed06ea83207c203468aec9da3dec2de9e5
Luqman Dekla <deklluqm@amazon.com>
2023-09-12 20:21:51 +0100

    Add tests that a user can delete their own account

--- user/tests.py
+++ user/tests.py
@@ -19,6 +19,12 @@ class UserTestCase(TestCase):
         )
         self.user.save()
         self.client.force_login(self.user)
+        self.another_user = User.objects.create(
+            username='test_user2',
+            email="JDoe2@email.com",
+            password='asdfasdf123123'
+        )
+        self.another_user.save()
 
     def test_that_first_name_cannot_have_numbers(self):
         self.user.first_name = '1'
@@ -48,14 +54,8 @@ class UserTestCase(TestCase):
         self.assertTemplateUsed(response, 'user/update_user_form.html')
 
     def test_that_an_authenticated_user_cannot_see_the_update_view_for_another_user(self):
-        another_user = User.objects.create(
-            username='test_user2',
-            email="JDoe2@email.com",
-            password='asdfasdf123123'
-        )
-        another_user.save()
 
-        response = self.client.get(reverse('user:update_user', args=[another_user.id]))
+        response = self.client.get(reverse('user:update_user', args=[self.another_user.id]))
         self.assertEqual(response.status_code, 403)
         self.assertTemplateNotUsed(response, 'user/update_user_form.html')
 
@@ -65,3 +65,18 @@ class UserTestCase(TestCase):
         self.assertTemplateNotUsed(response, 'user/update_user_form.html')
         self.assertTemplateUsed(response, 'registration/login.html')
 
+    def test_that_an_authenticated_user_can_see_the_delete_confirmation_for_himself(self):
+        response = self.client.get(reverse('user:delete_user', args=[self.user.id]))
+        self.assertEqual(response.status_code, 200) # due to the redirect defined by success_url
+        self.assertTemplateUsed(response, 'user/user_confirm_delete.html')
+
+    def test_that_an_authenticated_user_can_delete_his_own_account(self):
+        response = self.client.post(reverse('user:delete_user', args=[self.user.id]))
+        self.assertEqual(response.status_code, 302)
+        with self.assertRaises(User.DoesNotExist):
+            User.objects.get(id=self.user.id)
+
+    def test_that_an_authenticated_user_cannot_see_the_delete_confirmation_for_another(self):
+        response = self.client.get(reverse('user:delete_user', args=[self.another_user.id]))
+        self.assertEqual(response.status_code, 403) # due to the redirect defined by success_url
+        self.assertTemplateNotUsed(response, 'user/user_confirm_delete.html')
