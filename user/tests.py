345ec45deadf525b2d7d4607aadf692418160761
Luqman Dekla <deklluqm@amazon.com>
2023-09-12 21:29:11 +0100

    Add test that an authenticated user can update their username

--- user/tests.py
+++ user/tests.py
@@ -9,6 +9,7 @@ from .models import User
 
 # TODO: add comments of all the test cases you're NOT covering because django covers them for you
 
+
 class UserTestCase(TestCase):
     def setUp(self):
         self.client = Client()
@@ -53,8 +54,50 @@ class UserTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'user/update_user_form.html')
 
-    def test_that_an_authenticated_user_cannot_see_the_update_view_for_another_user(self):
+    def test_that_an_authenticated_user_can_update_his_username(self):
+        updated_details = {
+            'username': 'new_username',
+        }
+        self.assertNotEqual(self.user.username, updated_details['username'])
 
+        response = self.client.post(reverse('user:update_user', args=[self.user.id]), updated_details)
+        print(response.content)
+        self.assertEqual(response.status_code, 302)
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.username, updated_details['username'])
+
+    # def test_that_an_authenticated_user_cannot_set_his_username_to_empty(self):
+
+
+    # def test_that_a_user_cannot_set_his_username_to_another_username_that_is_in_use(self):
+
+    # def test_that_an_authenticated_user_can_update_his_first_name_to_a_new_value(self):
+    #     updated_details = {
+    #         'username':
+    #     }
+    #
+    # def test_that_an_authenticated_user_can_update_his_first_name_to_empty(self):
+    #     updated_details = {
+    #         'username':
+    #     }
+    #
+    #
+    # def test_that_an_authenticated_user_can_update_his_last_name_to_a_new_value(self):
+    #     updated_details = {
+    #         'username':
+    #     }
+    #
+    # def test_that_an_authenticated_user_can_update_his_first_name_to_empty(self):
+    #     updated_details = {
+    #         'username':
+    #     }
+    #
+    # def test_that_an_authenticated_user_can_update_his_email(self):
+    #     updated_details = {
+    #         'username':
+    #     }
+
+    def test_that_an_authenticated_user_cannot_see_the_update_view_for_another_user(self):
         response = self.client.get(reverse('user:update_user', args=[self.another_user.id]))
         self.assertEqual(response.status_code, 403)
         self.assertTemplateNotUsed(response, 'user/update_user_form.html')
@@ -63,6 +106,10 @@ class UserTestCase(TestCase):
         self.client.logout()
         response = self.client.get(reverse('user:update_user', args=[1]), follow=True)
         self.assertTemplateNotUsed(response, 'user/update_user_form.html')
+
+    def test_that_an_unauthenticated_user_is_redirected_to_login_when_trying_to_update_a_user(self):
+        self.client.logout()
+        response = self.client.get(reverse('user:update_user', args=[1]), follow=True)
         self.assertTemplateUsed(response, 'registration/login.html')
 
     def test_that_an_authenticated_user_can_see_the_delete_confirmation_for_himself(self):
@@ -75,6 +122,11 @@ class UserTestCase(TestCase):
         self.assertEqual(response.status_code, 302)
         with self.assertRaises(User.DoesNotExist):
             User.objects.get(id=self.user.id)
+
+    def test_that_a_user_is_redirected_after_a_successful_account_deletion(self):
+        response = self.client.post(reverse('user:delete_user', args=[self.user.id]), follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'user/users.html')
 
     def test_that_an_authenticated_user_cannot_see_the_delete_confirmation_for_another(self):
         response = self.client.get(reverse('user:delete_user', args=[self.another_user.id]))
@@ -96,3 +148,10 @@ class UserTestCase(TestCase):
         self.client.logout()
         response = self.client.post(reverse('user:delete_user', args=[self.another_user.id]), follow=True)
         self.assertTrue(User.objects.filter(id=self.another_user.id).exists())
+
+    def test_that_an_unauthenticated_user_is_redirected_after_attempting_to_delete_another_user(self):
+        self.client.logout()
+        response = self.client.post(reverse('user:delete_user', args=[self.user.id]), follow=True)
+        self.assertTemplateUsed(response, 'registration/login.html')
+        self.assertEqual(response.status_code, 200)
+
