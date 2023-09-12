b5659c9b5318f12549ba9780afbb1b6a6d70438e
Luqman Dekla <deklluqm@amazon.com>
2023-09-12 17:56:27 +0100

    Confirm list view displays list of movies

--- movie/tests.py
+++ movie/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Movie
+
+
+class MovieTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_movie_list_view(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'movie/index.html')
