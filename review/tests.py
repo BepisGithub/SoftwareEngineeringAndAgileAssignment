9b8dbc9fb7e619f942e88ce5803b4c2444bdf68d
Luqman Dekla <deklluqm@amazon.com>
2023-09-13 19:58:09 +0100

    Create initial update tests for review app

--- review/tests.py
+++ review/tests.py
@@ -195,6 +195,124 @@ class ReviewTestCase(TestCase):
 
     # Update tests
 
+    def create_valid_review_for_movie(self, movie_id):
+        response = self.client.post(reverse('review:create_movie_review', args=[movie_id]), self.valid_review)
+        return response
+
+    def test_that_an_authenticated_user_can_see_the_update_view_for_his_review(self):
+        self.create_valid_review_for_movie(self.movie1.id)
+        response = self.client.get(reverse('review:update', args=[self.movie1.id, 1]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'review/review_form.html')
+
+    # def test_that_an_authenticated_user_can_update_his_reviews_title(self):
+    #     updated_details = {
+    #         'username': 'new_username',
+    #     }
+    #     self.assertNotEqual(self.user.username, updated_details['username'])
+    #
+    #     response = self.client.post(reverse('user:update_user', args=[self.user.id]), updated_details)
+    #     self.assertEqual(response.status_code, 302)
+    #     self.user.refresh_from_db()
+    #     self.assertEqual(self.user.username, updated_details['username'])
+    #
+    # def test_that_an_authenticated_user_cannot_update_his_reviews_title_to_empty(self):
+    #     updated_details = {
+    #         'username': '',
+    #     }
+    #     response = self.client.post(reverse('user:update_user', args=[self.user.id]), updated_details)
+    #     self.user.refresh_from_db()
+    #     self.assertNotEqual(self.user.username, updated_details['username'])
+    #
+    #
+    # def test_that_an_authenticated_user_can_update_his_reviews_message(self):
+    #     updated_details = {
+    #         'first_name': 'newname'
+    #     }
+    #     response = self.client.post(reverse('user:update_user', args=[self.user.id]), updated_details)
+    #     self.user.refresh_from_db()
+    #     self.assertNotEqual(self.user.username, updated_details['first_name'])
+    #
+    # def test_that_an_authenticated_user_cannot_update_his_reviews_message_to_empty(self):
+    #     updated_details = {
+    #         'username': '',
+    #     }
+    #     response = self.client.post(reverse('user:update_user', args=[self.user.id]), updated_details)
+    #     self.user.refresh_from_db()
+    #     self.assertNotEqual(self.user.username, updated_details['username'])
+
+    # def test_that_an_authenticated_user_can_update_his_reviews_rating(self):
+    #     updated_details = {
+    #         'first_name': 'newname'
+    #     }
+    #     response = self.client.post(reverse('user:update_user', args=[self.user.id]), updated_details)
+    #     self.user.refresh_from_db()
+    #     self.assertNotEqual(self.user.username, updated_details['first_name'])
+    #
+    # def test_that_an_authenticated_user_cannot_update_his_reviews_rating_to_empty(self):
+    #     updated_details = {
+    #         'username': '',
+    #     }
+    #     response = self.client.post(reverse('user:update_user', args=[self.user.id]), updated_details)
+    #     self.user.refresh_from_db()
+    #     self.assertNotEqual(self.user.username, updated_details['username'])
+    # def test_that_an_authenticated_user_cannot_update_his_reviews_rating_to_less_than_one(self):
+    #     updated_details = {
+    #         'username': '',
+    #     }
+    #     response = self.client.post(reverse('user:update_user', args=[self.user.id]), updated_details)
+    #     self.user.refresh_from_db()
+    #     self.assertNotEqual(self.user.username, updated_details['username'])
+    # def test_that_an_authenticated_user_cannot_update_his_reviews_rating_to_greater_than_five(self):
+    #     updated_details = {
+    #         'username': '',
+    #     }
+    #     response = self.client.post(reverse('user:update_user', args=[self.user.id]), updated_details)
+    #     self.user.refresh_from_db()
+    #     self.assertNotEqual(self.user.username, updated_details['username'])
+    # def test_that_an_authenticated_user_cannot_update_his_reviews_rating_to_a_decimal(self):
+    #     updated_details = {
+    #         'username': '',
+    #     }
+    #     response = self.client.post(reverse('user:update_user', args=[self.user.id]), updated_details)
+    #     self.user.refresh_from_db()
+    #     self.assertNotEqual(self.user.username, updated_details['username'])
+    # def test_that_an_authenticated_user_cannot_update_his_reviews_rating_to_an_alphabetical_value(self):
+    #     updated_details = {
+    #         'username': '',
+    #     }
+    #     response = self.client.post(reverse('user:update_user', args=[self.user.id]), updated_details)
+    #     self.user.refresh_from_db()
+    #     self.assertNotEqual(self.user.username, updated_details['username'])
+    #
+    #
+    # def test_that_an_authenticated_user_is_redirected_back_to_the_update_form_on_unsuccessful_update(self):
+    #     updated_details = {
+    #         'first_name': '1',
+    #     }
+    #     response = self.client.post(reverse('user:update_user', args=[self.user.id]), updated_details)
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertTemplateUsed('user/update_user_form.html')
+    #     self.assertIn('form', response.context)
+    #     self.assertFalse(response.context['form'].is_valid())
+    #
+    # def test_that_an_authenticated_user_cannot_see_the_update_view_for_another_users_review(self):
+    #     response = self.client.get(reverse('user:update_user', args=[self.another_user.id]))
+    #     self.assertEqual(response.status_code, 403)
+    #     self.assertTemplateNotUsed(response, 'user/update_user_form.html')
+    #
+    # def test_that_an_authenticated_user_cannot_update_another_users_review(self):
+    #
+    # def test_that_an_unauthenticated_user_cannot_see_the_update_view_for_another_user(self):
+    #     self.client.logout()
+    #     response = self.client.get(reverse('user:update_user', args=[1]), follow=True)
+    #     self.assertTemplateNotUsed(response, 'user/update_user_form.html')
+    #
+    # def test_that_an_unauthenticated_user_is_redirected_to_login_when_trying_to_update_a_user(self):
+    #     self.client.logout()
+    #     response = self.client.post(reverse('user:update_user', args=[1]), follow=True)
+    #     self.assertTemplateUsed(response, 'registration/login.html')
+
     # we need to make sure only the author can both see the update form and update (take reference from user/tests.py
     # and then we need to validate the fields of the new review, and test for success and failure and where they both
     # redirect (again, take reference from the user tests)
