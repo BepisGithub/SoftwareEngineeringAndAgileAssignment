c7c107069dded26d671b15c08113192d601ca5d3
Luqman Dekla <deklluqm@amazon.com>
2023-09-13 22:02:40 +0100

    Add tests for movie model

--- movie/tests.py
+++ movie/tests.py
@@ -5,6 +5,8 @@ from datetime import datetime, timedelta
 
 from .models import Movie
 
+# Relatively few tests are required for this since there is no way for any user (apart from the site owner) to create,
+# update or delete any
 
 class MovieTestCase(TestCase):
     def setUp(self):
@@ -39,3 +41,26 @@ class MovieTestCase(TestCase):
 
         with self.assertRaises(ValidationError):
             self.movie.full_clean()
+
+    def test_that_movie_description_cannot_be_empty(self):
+        self.movie.description = ''
+
+        with self.assertRaises(ValidationError):
+            self.movie.full_clean()
+
+    def test_that_movie_duration_cannot_be_empty(self):
+        self.movie.duration = None
+
+        with self.assertRaises(ValidationError):
+            self.movie.full_clean()
+
+    def test_that_movie_date_released_cannot_be_empty(self):
+        self.movie.date_released = None
+
+        with self.assertRaises(ValidationError):
+            self.movie.full_clean()
+
+    def test_that_movie_rating_can_be_empty(self):
+        self.movie.average_rating_out_of_five = None
+
+        self.movie.full_clean()
