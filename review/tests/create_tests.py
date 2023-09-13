7c90c588f639492954165fcd10b4dc74c5283959
Luqman Dekla <deklluqm@amazon.com>
2023-09-13 23:45:46 +0100

    Refactor review tests to use test utils to reduce repetition

--- review/tests/create_tests.py
+++ review/tests/create_tests.py
@@ -11,51 +11,10 @@ from user.models import User
 
 from review.models import Review
 
-class CreateReviewTestCase(TestCase):
-    def setUp(self):
-        self.client = Client()
-        self.user1 = User.objects.create(
-            username='test_user',
-            email="JDoe@email.com",
-            password='asdfasdf123123'
-        )
-        self.user1.save()
-        self.client.force_login(self.user1)
+from review.tests.test_utils import BaseTestCase
 
-        self.user2 = User.objects.create(
-            username='second_test_user',
-            email="DJoe@email.com",
-            password='asdfasdf123123'
-        )
-        self.user2.save()
 
-        self.movie1 = Movie.objects.create(
-            id=1,
-            title='Test Movie',
-            description='Test Description',
-            duration=timedelta(hours=3),
-            date_released=datetime.today(),
-            average_rating_out_of_five=None
-        )
-
-        self.movie1.save()
-
-        self.movie2 = Movie.objects.create(
-            id=2,
-            title='Second Test Movie',
-            description='Second Test Description',
-            duration=timedelta(hours=3),
-            date_released=datetime.today(),
-            average_rating_out_of_five=None
-        )
-
-        self.movie2.save()
-
-        self.valid_review = {
-            'title': 'review title',
-            'message': 'review message',
-            'rating_out_of_five': 5,
-        }
+class CreateReviewTestCase(BaseTestCase):
 
     def test_that_an_authenticated_user_can_see_the_create_view_if_its_their_first_review_of_a_movie(self):
         response = self.client.get(reverse('review:create_movie_review', args=[self.user1.id]))
