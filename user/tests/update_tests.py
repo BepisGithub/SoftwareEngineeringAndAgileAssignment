7665846562127943be5ce69a851f837c249a8905
Luqman Dekla <deklluqm@amazon.com>
2023-09-19 01:26:21 +0100

    Fix user update tests

--- user/tests/update_tests.py
+++ user/tests/update_tests.py
@@ -4,6 +4,8 @@ from django.urls import reverse
 
 from user.tests.test_utils import BaseTestCase
 
+from user.tests.test_utils import get_valid_account_details, get_invalid_account_details
+
 
 class UpdateUserTestCase(BaseTestCase):
 
@@ -13,82 +15,71 @@ class UpdateUserTestCase(BaseTestCase):
         self.assertTemplateUsed(response, 'user/update_user_form.html')
 
     def test_that_an_authenticated_user_can_update_their_username(self):
-        updated_details = {
-            'username': 'new_username',
-        }
+        updated_details = get_valid_account_details()
+        updated_details['username'] = 'new_username'
         self.assertNotEqual(self.user.username, updated_details['username'])
         response = self.client.post(reverse('user:update', args=[self.user.id]), updated_details)
         self.user.refresh_from_db()
         self.assertEqual(self.user.username, updated_details['username'])
 
     def test_that_an_authenticated_user_cannot_set_their_username_to_empty(self):
-        updated_details = {
-            'username': '',
-        }
+        updated_details = get_valid_account_details()
+        updated_details['username'] = ''
         response = self.client.post(reverse('user:update', args=[self.user.id]), updated_details)
         self.user.refresh_from_db()
         self.assertNotEqual(self.user.username, updated_details['username'])
 
     def test_that_a_user_cannot_set_their_username_to_another_username_that_is_in_use(self):
-        updated_details = {
-            'username': self.another_user.username,
-        }
+        updated_details = get_valid_account_details()
+        updated_details['username'] = self.another_user.username
         response = self.client.post(reverse('user:update', args=[self.user.id]), updated_details)
         self.user.refresh_from_db()
         self.assertNotEqual(self.user.username, updated_details['username'])
 
     def test_that_an_authenticated_user_can_update_their_first_name_to_a_new_value(self):
-        updated_details = {
-            'first_name': 'newname'
-        }
+        updated_details = get_valid_account_details()
+        updated_details['first_name'] = 'newname'
         response = self.client.post(reverse('user:update', args=[self.user.id]), updated_details)
         self.user.refresh_from_db()
-        self.assertEqual(self.user.username, updated_details['first_name'])
+        self.assertEqual(self.user.first_name, updated_details['first_name'])
 
     def test_that_an_authenticated_user_can_update_their_first_name_to_empty(self):
-        updated_details = {
-            'first_name': ''
-        }
+        updated_details = get_valid_account_details()
+        updated_details['first_name'] = ''
         response = self.client.post(reverse('user:update', args=[self.user.id]), updated_details)
         self.user.refresh_from_db()
-        self.assertEqual(self.user.username, updated_details['first_name'])
+        self.assertEqual(self.user.first_name, updated_details['first_name'])
 
     def test_that_an_authenticated_user_can_update_their_last_name_to_a_new_value(self):
-        updated_details = {
-            'last_name': 'newname'
-        }
+        updated_details = get_valid_account_details()
+        updated_details['last_name'] = 'newname'
         response = self.client.post(reverse('user:update', args=[self.user.id]), updated_details)
         self.user.refresh_from_db()
-        self.assertEqual(self.user.username, updated_details['last_name'])
+        self.assertEqual(self.user.last_name, updated_details['last_name'])
 
     def test_that_an_authenticated_user_can_update_their_last_name_to_empty(self):
-        updated_details = {
-            'last_name': ''
-        }
+        updated_details = get_valid_account_details()
+        updated_details['last_name'] = ''
         response = self.client.post(reverse('user:update', args=[self.user.id]), updated_details)
         self.user.refresh_from_db()
-        self.assertEqual(self.user.username, updated_details['last_name'])
+        self.assertEqual(self.user.last_name, updated_details['last_name'])
 
     def test_that_an_authenticated_user_can_update_their_email(self):
-        updated_details = {
-            'email': 'new@email.com'
-        }
+        updated_details = get_valid_account_details()
+        updated_details['email'] = 'new@email.com'
         response = self.client.post(reverse('user:update', args=[self.user.id]), updated_details)
         self.user.refresh_from_db()
-        self.assertEqual(self.user.username, updated_details['email'])
+        self.assertEqual(self.user.email, updated_details['email'])
 
     def test_that_an_authenticated_user_cannot_update_their_email_to_empty(self):
-        updated_details = {
-            'email': ''
-        }
+        updated_details = get_valid_account_details()
+        updated_details['email'] = ''
         response = self.client.post(reverse('user:update', args=[self.user.id]), updated_details)
         self.user.refresh_from_db()
         self.assertNotEqual(self.user.email, updated_details['email'])
 
     def test_that_an_authenticated_user_is_redirected_back_to_the_update_form_on_unsuccessful_update(self):
-        updated_details = {
-            'first_name': '1',
-        }
+        updated_details = get_invalid_account_details()
         response = self.client.post(reverse('user:update', args=[self.user.id]), updated_details)
 
         # In Django, unsuccessful updates cause the view to rerender the form which means no redirect thus a 200 code
